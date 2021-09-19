af6327a7b9f7df862b3c2ce34cf572b4abdaa3aa
manoj luintel <luintel3@gmail.com>
2021-09-18 22:10:35 -0400

    ip change

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '18.208.194.197',
+      host: '54.163.31.189',
       username: 'ec2-user',
       privateKey: 'hs-key-final.pem'
     })
